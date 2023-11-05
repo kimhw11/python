612d57e8509a78fe7a4a69f8292d651068a43204
kimhw11 <128278274+kimhw11@users.noreply.github.com>
2023-11-05 21:40:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,29 +14,28 @@ while True:
     # 4. 사용자 자리 입력(올바른 자리 확인) > inputPOS()
     pos = input_POS(board)
     board[pos] = player
+
     # 5. 보드판 출력 ---------------------------------------
     display_board(board)
 
     # 6. 우승 조건 확인 - 우승 조건 성립 > 플레이어 승 출력 후 게임 종료 > victory()
-    if Victory():
+    if Victory(board , player):
         print("Player win")
         exit()
 
+    if draw(board):
+        print("무승부")
+        exit()
+
     # 7. 컴퓨터 입력(랜덤한 정수 만들어 해당 자리에 문양 삽입) > randomPOS()
-    pos = input_POS(board)
-    board[pos] = computer
+    sp = random_POS(board , player)
+    board[sp] = computer
 
     # 8. 보드판 출력 ---------------------------------------
     display_board(board)
 
     # 9. 컴퓨터 우승 인지 확인 > victory()
     # 10. 컴퓨터가 우승 조건 성립시 컴퓨터 승 출력 후 게임 종료
-    if Victory():
+    if Victory(board , computer):
         print('Computer win')
         exit()
-
-
-
-
-
-
